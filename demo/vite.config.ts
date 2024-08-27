bf2d4ae500f311eda55da74520b1cd7e84b5271c
Nathaniel Baughman <neb@nebaughman.com>
2024-08-27 16:48:11 -0400

    Update demo to build to /docs (set base for GitHub Pages)

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -8,5 +8,10 @@ export default defineConfig({
   plugins: [vue()],
   define: {
     LOW_CAL_LIB_VERSION: JSON.stringify(libPackage.version),
-  }
+  },
+  base: "/low-cal/", // https://vitejs.dev/guide/static-deploy.html#github-pages
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+  },
 })
